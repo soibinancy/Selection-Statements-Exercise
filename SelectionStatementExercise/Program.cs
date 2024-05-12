28eb23dd7b7ddecd75ef9211b49085075d605d75
Soibi Nancy <39niches@gmail.com>
2024-05-12 03:23:33 -0500

    selection exercise 1 complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace SelectionStatementExercise
+﻿
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random r = new Random();
+            int favNumber = r.Next(1, 10);
+            int userInput = 0;
+
+            while(userInput != favNumber)
+            {
+                
+            }
+            Console.WriteLine("Guess my favorite number (1:");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"Too low!\nYour guess: {userInput}");
+            }
+            else if(userInput > favNumber)
+            {
+                Console.WriteLine($"Too high!\nYour guess: {userInput}");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it!\nYour guess: {userInput}");
+            }
         }
     }
 }
